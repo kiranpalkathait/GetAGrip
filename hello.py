b604ac13b19c3b662584b82d02371e23035f4187
Jonathan Humphrey Bodian <jbodian@gmail.com>
2013-07-09 00:59:59 -0400

    playing with decorators

--- hello.py
+++ hello.py
@@ -4,6 +4,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+@app.route('/helloderp')
+def derp_page():
+    return 'Derp'
 
 if __name__ == '__main__':
     app.run()
