dd23ac1a383ce5398aa8c6426b4889029fc5763d
Jonathan Humphrey Bodian <jbodian@gmail.com>
2013-07-09 11:15:13 -0400

    More stuff. Login thing that doesnt work

--- hello.py
+++ hello.py
@@ -13,6 +13,13 @@ def projects():
 def about():
     return'The about page'
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        do_the_login()
+    else:
+        show_the_login_form()
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
 
