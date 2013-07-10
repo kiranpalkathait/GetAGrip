b93185b13fb9e2fb6d97ba09bed71f474667eb94
Jonathan Humphrey Bodian <jbodian@gmail.com>
2013-07-10 15:05:58 -0400

    first commit

--- routes.py
+++ routes.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def derp():
     return render_template('derp.html')
 
 if __name__ == '__main__':
