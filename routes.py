f2b18a69ec529089c1cd147dce64195014c4f1d5
Jonathan Humphrey Bodian <jbodian@gmail.com>
2013-07-11 00:39:49 -0400

    changed routes to new home.html. (PLEASE work >.<)

--- routes.py
+++ routes.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def derp():
-    return render_template('derp.html')
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run()
