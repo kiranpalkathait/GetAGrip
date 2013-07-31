8127b20822646569009f6ec2afbef49e0278a2ad
Jonathan Bodian <jonbodian@new-host-2.home>
2013-07-31 00:09:05 -0400

    added comments in database

--- app/models.py
+++ app/models.py
@@ -8,7 +8,16 @@ class User(db.Model):
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(100), index=True, unique=True)
     role = db.Column(db.SmallInteger, default=ROLE_USER)
+    posts = db.relationship('Comments', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
     
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post %r>' % (self.body)
