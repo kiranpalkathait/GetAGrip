cc7bd44995cffd4f6060ebf5b17e6109c531b569
Jacob Henner <code@ventricle.us>
2013-11-04 01:54:23 -0500

    Adding a .gitignore, removing things that shouldn't be in the repository.

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-CSRF_ENABLED = True
-SECRET_KEY = 'Ultr@viole1'
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
